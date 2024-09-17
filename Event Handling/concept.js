cdafd1f62a76fa959d83a7f20e7192731147ec89
RgnDunes <divyansh.singh@razorpay.com>
2024-09-18 03:05:23 +0530

    add session 7

---
+++ Event Handling/concept.js
@@ -0,0 +1,12 @@
+// element.addEventListener('event', function);
+
+// event propagation
+// event bubbling
+
+// event object
+
+// keyboard events - keydown, keyup, keypress etc etc
+// mouse events - click, double click etc etc
+// form events - submit
+
+// debouncing and throttling events
